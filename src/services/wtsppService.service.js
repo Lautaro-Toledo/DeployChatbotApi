a40bfcbfc31ade17e50154239fd38eb197eb7bb3
lautarohelp <lautarotoledo391@gmail.com>
2024-05-22 13:20:01 -0300

    arreglo de importaciones

--- src/services/wtsppService.service.js
+++ src/services/wtsppService.service.js
@@ -1,5 +1,5 @@
 import https from "https";
-import { Process } from "../shared/processMessage";
+import { MessageText, messageButtons } from "../shared/whatsApp.modes.js";
 
 export class WtsppService {
   VerifyToken = ( req, res) => {
@@ -35,7 +35,7 @@ export class WtsppService {
         if (text !== "") {
           console.log(text);
           console.log(number);
-          await Process(text, number)
+          await Process(text, number);
         }
       }
       res.send("EVENT_RECEIVED")
@@ -91,6 +91,55 @@ export class WtsppService {
   
     req.write(data);
     req.end();
+  }
+
+  Process = async (textUser, number)=> {
+    textUser= textUser.toLowerCase();
+    let models = [];
+  
+  
+    //#region sin gemini
+    //Hola que tal
+    if (textUser.includes("hola")) {
+      //SALUDAR
+      const model = MessageText("hello, nice to me you", number);
+      models.push(model);
+      const listModel = MessageText("you are welcome", number);
+      models.push(listModel);
+    } else if (textUser.includes("gracias")) {
+      const model = MessageText("you are welcome", number);
+      models.push(model);
+    } else if (textUser.includes("adios")) {
+      const model = MessageText("bye bye", number);
+      models.push(model);
+    } else if (textUser.includes("comprar")) {
+      const model = messageButtons("Que quieres comprar?", number);
+      models.push(model);
+    } else if (textUser.includes("vender")) {
+      const model = MessageText("puedes vender x aca", number);
+      models.push(model);
+    } else {
+      const model =  MessageText("I do not know", number);
+      models.push(model);
+    }
+    // #endregion
+  
+    //#region con gemini
+    // const responseChatGPT = await chatGPTServices.GetMessageChatGPT(textUser)
+    // if (responseChatGPT !== null) {
+    //   const model = MessageText(responseChatGPT, number);
+    //   models.push(model)
+    // } else {
+    //   const model = wtsppModels.MessageText("Lo siento algo salio mal intesta mas tarde", number);
+    //   models.push(model)
+    // }
+    
+    // #endregion
+    
+  
+    models.forEach(model => {
+      this.SendMessageWtspp(model);
+    })
   }
   
 }
